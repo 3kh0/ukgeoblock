217b2fa123cfc431cbdfee988998c9c8d938eaa6
Echo <github@3kh0.net>
2025-10-18 18:33:49 -0400

    info

--- app/app.vue
+++ app/app.vue
@@ -1,5 +1,26 @@
 <template>
-  <div>
-    <h1 class="text-4xl font-bold text-center">Sorry, this site is not available in your country.</h1>
-  </div>
+  <main class="flex flex-col justify-between items-center mt-15 py-8">
+    <h1 class="text-5xl font-bold text-center px-4 w-full max-w-5xl mb-12">
+      Sorry, this site is not available in your country due to Ofcom regulations
+    </h1>
+
+    <div class="px-4 text-2xl w-full max-w-5xl text-center space-y-4">
+      <p>
+        The UK has strict content regulations intended to protect children, but I argue their real purpose is to suppress free speech and creativity in one of the final bastions of freedom.
+      </p>
+
+      <p>
+        Sites such as 4chan and Kiwi Farms have challenged the UK's age‑verification law in court. Read more in their own 
+        <a href="https://kiwifarms.st/threads/4chan-and-kiwi-farms-sue-the-uk-over-its-age-verification-law.227364/" target="_blank" rel="noopener noreferrer" class="text-white underline">Kiwifarms thread</a>. The UK has already tried to <a href="https://www.theregister.com/2025/10/13/4chan_ofcom_fine/" target="_blank" rel="noopener noreferrer" class="text-white underline">fine 4chan £20,000</a> and claimed that the <a href="https://reclaimthenet.org/uk-ofcom-claims-first-amendment-cant-shield-americans" target="_blank" rel="noopener noreferrer" class="text-white underline">1st amendment does not protect Americans from their censorship laws</a>. This is bordering on censorship.
+      </p>
+
+      <blockquote class="italic">"I disapprove of what you say, but I will defend to the death your right to say it." - Voltaire</blockquote>
+
+      <p>
+        You may also opt to use a VPN to access content not available in your region. I highly recommend <a href="https://mullvad.net/en" target="_blank" rel="noopener noreferrer" class="text-white underline">Mullvad</a> as they are one of the most trusted and secure VPN providers.
+      </p>
+
+      <p>I am still reachable on <a href="mailto:3kh0@3kh0.net" target="_blank" rel="noopener noreferrer" class="text-white underline">Email</a>, <a href="https://signal.me/#eu/EnC26yk3rkDfP2JPoguBZYjCVhMD6_xAGxu1L4UzhdK0bnQgDHdIQLCQjR6eBdZF" target="_blank" rel="noopener noreferrer" class="text-white underline">Signal</a>, and <a href="https://t.me/doober" target="_blank" rel="noopener noreferrer" class="text-white underline">Telegram</a>.</p>
+    </div>
+  </main>
 </template>
