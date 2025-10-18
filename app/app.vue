9d93a1f94632245a66e6613f1e28e36697c0431f
Echo <github@3kh0.net>
2025-10-18 18:00:45 -0400

    uk block

---
+++ app/app.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h1 class="text-4xl font-bold text-center">Sorry, this site is not available in your country.</h1>
+  </div>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,10 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  css: ['~/assets/main.css'],
+  devtools: { enabled: true },
+  vite: {
+    plugins: [tailwindcss()]
+  }
+})
